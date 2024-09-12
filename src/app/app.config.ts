3528d36205e5401498a78090f8b208f359811cf8
Seima <seima1421@gmail.com>
2024-09-12 01:34:35 -0300

    ajuste hash

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
-        provideAnimationsAsync()
+        provideAnimationsAsync(),
+        {
+            provide: LocationStrategy,
+            useClass: HashLocationStrategy
+        }
     ]
 };
